0697c0fe8aad637738d8720a436b0a1a25ab4bbc
魏新杰 <646367892@qq.com>
2018-08-24 16:14:09 +0800

    Initial Commit

---
+++ YLAudio/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YLAudio
+//
+//  Created by 魏新杰 on 2018/8/24.
+//  Copyright © 2018年 weixinjie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YLAudio/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YLAudio
+//
+//  Created by 魏新杰 on 2018/8/24.
+//  Copyright © 2018年 weixinjie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
